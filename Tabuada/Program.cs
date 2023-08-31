5c65df3ef0cda46feea8f6dcb393eb2ee8b3913f
Filipe Barreto <filipe.barreto@hotmail.com>
2023-08-31 16:18:58 +0100

    correção do for, variavel sendo declarada dentro a estrutura de repetição

--- Tabuada/Program.cs
+++ Tabuada/Program.cs
@@ -2,23 +2,18 @@
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Porfavor insira um número para receber a sua tabuada:");
+        Console.WriteLine("Por favor insira um número para receber a sua tabuada:");
         string numero_a_calcular_string = Console.ReadLine();
         double numero_a_calcular = double.Parse(numero_a_calcular_string);
-
-        int numero_tabuada = 1;
 
         Console.WriteLine("Agora insira o número de vezes que quer que seja repetido");
         string numero_de_vezes_string = Console.ReadLine();
         double numero_de_vezes = double.Parse(numero_de_vezes_string);
 
-
         Console.WriteLine("A tabuada deste número é :");
 
-
-        for (numero_tabuada = 1; numero_tabuada <= numero_de_vezes; numero_tabuada++)
+        for (int numero_tabuada = 1; numero_tabuada <= numero_de_vezes; numero_tabuada++)
         {
-
             double resultado = numero_a_calcular * numero_tabuada;
 
             Console.WriteLine(" " + numero_a_calcular + " X " + numero_tabuada + " = " + resultado);
